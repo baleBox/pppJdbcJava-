b08061d2e7b89ba745ef765d08869c075e693a5c
Alexander Stupin <astupin2012@gmail.com>
2024-02-29 20:16:51 +0300

    Third commit. Only for JDBC. No mapping. Green.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соединения с БД
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/baledbtest";
     private static final String DB_USERNAME = "rootroot";
